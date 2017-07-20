73477e34d1da0cb464bcf985fbfbc1909e4a29a4
Leonhardt Koepsell <leonhardt@koepsell.io>
2017-07-20 10:12:34 -0700

    Make JUnit only a testCompile dependency

--- results-test/src/main/kotlin/io/github/codebandits/results/helpers.kt
+++ results-test/src/main/kotlin/io/github/codebandits/results/helpers.kt
@@ -1,17 +1,17 @@
 package io.github.codebandits.results
 
-import org.junit.jupiter.api.Assertions.fail
+import java.lang.AssertionError
 
 infix fun <failureType, successType> Result<failureType, successType>.succeedsAnd(onSuccess: (successType) -> Unit) {
     when (this) {
         is Success -> onSuccess(this.content)
-        is Failure -> fail("Result should have been a Success")
+        is Failure -> throw AssertionError("Result should have been a Success")
     }
 }
 
 infix fun <failureType, successType> Result<failureType, successType>.failsAnd(onFailure: (failureType) -> Unit) {
     when (this) {
-        is Success -> fail("Result should have been a Failure")
+        is Success -> throw AssertionError("Result should have been a Failure")
         is Failure -> onFailure(this.content)
     }
 }
