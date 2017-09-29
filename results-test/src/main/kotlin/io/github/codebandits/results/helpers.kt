6a3b954ffa4f8a5ae43974670e8b5bbda80cdb3f
Leonhardt Koepsell <leonhardt@koepsell.io>
2017-09-29 13:50:55 -0700

    Make error messages more informative about Result contents

--- results-test/src/main/kotlin/io/github/codebandits/results/helpers.kt
+++ results-test/src/main/kotlin/io/github/codebandits/results/helpers.kt
@@ -5,27 +5,27 @@ import java.lang.AssertionError
 infix fun <failureType, successType> Result<failureType, successType>.succeedsAnd(onSuccess: (successType) -> Unit) {
     when (this) {
         is Success -> onSuccess(this.content)
-        is Failure -> throw AssertionError("Result should have been a Success")
+        is Failure -> throw AssertionError("Result should have been a Success but was a Failure with ${this.content}")
     }
 }
 
 infix fun <failureType, successType> Result<failureType, successType>.failsAnd(onFailure: (failureType) -> Unit) {
     when (this) {
-        is Success -> throw AssertionError("Result should have been a Failure")
+        is Success -> throw AssertionError("Result should have been a Failure but was a Success with ${this.content}")
         is Failure -> onFailure(this.content)
     }
 }
 
 infix fun <failureType, successType> Result<failureType, successType>.succeedsWith(expectedSuccess: Any) {
     when (this) {
-        is Success -> assert(this.content == expectedSuccess, { "Result should have been $expectedSuccess but was ${this.content}" })
-        is Failure -> throw AssertionError("Result should have been a Success")
+        is Success -> assert(this.content == expectedSuccess, { "Success should have been $expectedSuccess but was ${this.content}" })
+        is Failure -> throw AssertionError("Result should have been a Success but was a Failure with ${this.content}")
     }
 }
 
 infix fun <failureType, successType> Result<failureType, successType>.failsWith(expectedFailure: Any) {
     when (this) {
-        is Success -> throw AssertionError("Result should have been a Failure")
-        is Failure -> assert(this.content == expectedFailure, { "Result should have been $expectedFailure but was ${this.content}" })
+        is Success -> throw AssertionError("Result should have been a Failure but was a Success with ${this.content}")
+        is Failure -> assert(this.content == expectedFailure, { "Failure should have been $expectedFailure but was ${this.content}" })
     }
 }
